a4a622b4cffa13e6ec1f294da7b4b1e70633157c
avrilalelia <79146106+avrilalelia@users.noreply.github.com>
2024-05-12 14:42:41 +0700

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3"><i class="fa fa-viruses"></i> Covid Tracker</div>
+  </header>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Header'
+}
+</script>
